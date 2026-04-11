3d354f0a446d8a1bda8dea68d95283ec66770666
mirusm <mirka8889@gmail.com>
2026-04-11 23:35:41 +0200

    url fix

--- frontend/src/NewPredictionAkrPage.jsx
+++ frontend/src/NewPredictionAkrPage.jsx
@@ -28,7 +28,7 @@ const NewPredictionAkrPage = ({ onResults }) => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
     return localStorage.getItem("theme") === "dark";
   });
-  const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
+  const API_URL = import.meta.env.VITE_API_URL || "/api";
 
   useEffect(() => {
     const handleThemeChange = (e) => {
